e26ee0bc3471528a5890cd6843727ed5e96459ad
Ana Alice <anaalicefm18@gmail.com>
2020-09-06 20:58:23 -0300

    feat: task-3

---
+++ tasks-1/task-1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James bond"
+	z := true
+
+	fmt.Println("x: ", x, "\ny: ", y, "\nz: ", z)
+
+	fmt.Println("\nx: ", x)
+	fmt.Println("y: ", y)
+	fmt.Println("z: ", z)
+}
